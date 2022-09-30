07122f00283759e477545f5c11698df3a7762a69
Karl J. Overå <k@rl.run>
2022-09-30 08:32:44 +0200

    reduce concurrent requests, add more delay between threads

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const BASE_DOMAIN = 'https://dinesykmeldte.labs.nais.io';
 
 // Editable configuration
 export default {
-    ITERATIONS: 5,
-    CONCURRENT_REQUESTS: 5,
+    ITERATIONS: 100,
+    CONCURRENT_REQUESTS: 2,
     BASE_DOMAIN,
     URL: `${BASE_DOMAIN}/arbeidsgiver/sykmeldte`,
 };
